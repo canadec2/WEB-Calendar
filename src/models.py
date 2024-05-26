5078baea2d49262af2451b573b2d72917ab5a772
strafe <nik.education@mail.ru>
2024-05-26 19:04:27 +0300

    add_events_and_action_with_it

--- src/models.py
+++ src/models.py
@@ -10,3 +10,31 @@ class User:
 
     def __str__(self):
         return f"Пользователь {self.username} с электронной почтой {self.email}."
+
+
+class Event:
+
+    def __init__(
+        self, user_id, event_name, description, year, month, day, time, periodicity
+    ):
+        self.user_id = user_id
+        self.event_name = event_name
+        self.description = description
+        self.year = year
+        self.month = month
+        self.day = day
+        self.time = time
+        self.periodicity = periodicity
+
+    def get_event_info(self):
+        return {
+            "user_id": self.user_id,
+            "event_name": self.event_name,
+            "description": self.description,
+            "year": self.year,
+            "month": self.month,
+            "week": self.week,
+            "day": self.day,
+            "time": self.time,
+            "periodicity": self.periodicity,
+        }
