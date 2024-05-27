7cc867a6c7e1d6b1c112828a176ef54275aed976
canadec2 <2canadec@gmail.com>
2024-05-27 18:28:43 +0300

    new_structure

--- app.py
+++ app.py
@@ -1,27 +1,19 @@
+from flask import render_template, session, jsonify
+from http import HTTPStatus
 import connexion
 import os
 import sys
 import sqlite3
 import sqlite_query
-from flask import render_template, session
-from http import HTTPStatus
 from pathlib import Path
 
 
+# секретный ключ
 def generate_secret_key():
     return os.urandom(24)
 
-
-def init_db():
-    with sqlite3.connect(DATA_USERS_PATH) as conn:
-        cur = conn.cursor()
-        cur.execute(sqlite_query.create_table_users)
-        cur.execute(sqlite_query.create_table_events)
-        conn.commit()
-
-
 BASE_DIR = Path(__file__).resolve().parent
-DATA_USERS_PATH = BASE_DIR / "data.db"
+DATA_EVENTS_PATH = os.path.join(BASE_DIR, "data.db")
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 src_dir = os.path.join(current_dir, "src")
@@ -31,9 +23,15 @@ if src_dir not in sys.path:
 app = connexion.App(__name__, specification_dir="./")
 app.add_api("swagger.yml")
 
+# установка ключа
 app.app.secret_key = generate_secret_key()
 
-init_db()
+conn = sqlite3.connect(DATA_EVENTS_PATH)  # создаем базу данных, если ее еще нет
+cur = conn.cursor()
+
+@app.route("/")
+def index():
+    return render_template("authorization.html")  # главное окно с формой
 
 if __name__ == "__main__":
-    app.run("app:app", port=8000)
+    app.run(host="127.0.0.1", port=8086)
